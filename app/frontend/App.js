e35c6187d203db26565784d88126a3ab710bcacf
Chase <chasehamlinyw@gmail.com>
2026-06-27 16:48:46 -0400

    Add Home component with API call and routing

---
+++ app/frontend/App.js
@@ -0,0 +1,57 @@
+import { useEffect } from "react";
+import "@/App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import api from "@/api";
+import { HOME } from "@/constants/testIds";
+
+const Home = () => {
+  const helloWorldApi = async () => {
+    try {
+      const response = await api.get(`/`);
+      console.log(response.data.message);
+    } catch (e) {
+      console.error(e, `errored out requesting / api`);
+    }
+  };
+
+  useEffect(() => {
+    helloWorldApi();
+  }, []);
+
+  return (
+    <div>
+      <header className="App-header">
+        <a
+          data-testid={HOME.emergentLink}
+          className="App-link"
+          href="https://emergent.sh"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img
+            data-testid="home-logo"
+            src="https://avatars.githubusercontent.com/in/1201222?s=120&u=2686cf91179bbafbc7a71bfbc43004cf9ae1acea&v=4"
+            alt="Emergent"
+          />
+        </a>
+        <p data-testid="home-splash" className="mt-5">Building something incredible ~!</p>
+      </header>
+    </div>
+  );
+};
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />}>
+            <Route index element={<Home />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
